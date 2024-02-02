226c945d9899ba7c96efb82b0ccddb74aa2def5c
Qian Tang <qian@itrazotracetech.com>
2024-02-02 22:35:34 +1100

    coments

---
+++ src/Comments.kt
@@ -0,0 +1,9 @@
+fun main() {
+//    println("Hello world, comment in kotlin")
+/*    println("Hello world, comment in multiple lines")
+    println("Hello world, comment in multiple lines")
+    hhhh
+    odkdkdk
+    println("Hello world, comment in multiple lines")
+ */
+}
