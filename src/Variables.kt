72f5191a9b5e3ceb421ca0611b7e1101e6c5dcba
Qian Tang <qian@itrazotracetech.com>
2024-02-02 22:48:52 +1100

    variable

---
+++ src/Variables.kt
@@ -0,0 +1,8 @@
+fun main() {
+//    var name: String = "Amy"
+//    var age: Int = 19
+//    println("Name: ${name}, Age: ${age}")
+    val name = "Amy"
+    val age = 19
+    println("Name: ${name}, Age: ${age}")
+}
