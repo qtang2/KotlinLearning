616c9ce00924d94943dddbb62d0269611b58bcc0
Qian Tang <qian@itrazotracetech.com>
2024-02-02 22:56:11 +1100

    datatype

---
+++ src/DataType.kt
@@ -0,0 +1,11 @@
+fun main() {
+//    warning will be here as the explicitly given type is redundant
+    val n: Int = 1
+    val l: Long = 100L
+    val d: Double = 3.14
+    val f: Float = 3.15F
+    val s: String = "double quote for string, single quote for char"
+    val c: Char = 's'
+
+
+}
--- src/Variables.kt
+++ src/Variables.kt
@@ -2,6 +2,7 @@ fun main() {
 //    var name: String = "Amy"
 //    var age: Int = 19
 //    println("Name: ${name}, Age: ${age}")
+//    Variable is never modified, so it can be declared using 'val'
     val name = "Amy"
     val age = 19
     println("Name: ${name}, Age: ${age}")
