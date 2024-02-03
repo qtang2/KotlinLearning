0b3cb2ddbb5a10d4a4c12ff9511f22b6398002b5
Qian Tang <qian@itrazotracetech.com>
2024-02-03 22:06:56 +1100

    string ref

--- src/String.kt
+++ src/String.kt
@@ -20,4 +20,16 @@ fun main() {
     println(email.format("Amy"))
     println(msg.format("OKK"))
 
+    val name1 = "Amy"
+    val name2 = "Tom"
+    val name3 = "Amy"
+//    the name 1 and name 2 are stored in the string pool, when name 3 comes, it will check the string pool, if there
+//    one string with same content already exist, then it will point to the same string
+    val name4 = String("Amy".toCharArray()) // create a brand-new string object
+    println("== : ${name1 == name3}") // return true as compare the content
+    println(".equals ${name1 == name3}") // return true as compare the content
+    println("=== : ${name1 === name3}")  // return true as compare the memory reference
+    println("=== : ${name1 === name4}") // return false as compare the memory reference
+
+
 }
