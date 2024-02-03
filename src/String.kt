94236186eb72e0e3262a978f890c01ca5ce36c4f
Qian Tang <qian@itrazotracetech.com>
2024-02-03 21:13:51 +1100

    string

---
+++ src/String.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val name = "Anmdndlfd"
+    println(name.uppercase())
+    println(name.lowercase())
+    println(name.length)
+    println(name[3])
+    println(name.isEmpty()) // return false
+    println("".isEmpty()) // return true
+
+}
