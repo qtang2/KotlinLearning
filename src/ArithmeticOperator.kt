a0f85fa8d22fb8c0e84283e8c61e80f476cc7f55
Qian Tang <qian@itrazotracetech.com>
2024-02-03 22:22:04 +1100

    ++ --

--- src/ArithmeticOperator.kt
+++ src/ArithmeticOperator.kt
@@ -1,4 +1,9 @@
 
+import kotlin.math.PI
+import kotlin.math.max
+import kotlin.math.min
+//import kotlin.math.*
+
 fun main() {
     val number1 = 10
     val number2 = 3
@@ -7,4 +12,16 @@ fun main() {
     println("* ${number1*number2}")
     println("/ ${number1/number2}")
     println("% ${number1%number2}")
+    println(PI)
+    println(max(20,40))
+    println(min(20,40))
+
+    var number = 10
+    println(++number) // increase number and return the value, 11
+    println(number++) // return the value, then increase the number 11
+    println(number) // 12
+
+    println(--number) // increase number and return the value, 11
+    println(number--) // return the value, then increase the number 11
+    println(number) // 10
 }
