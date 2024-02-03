8753dbaca310ca50e4ddf7c31a9119cd5e224826
Qian Tang <qian@itrazotracetech.com>
2024-02-03 22:46:48 +1100

    logic operator

---
+++ src/Boolean.kt
@@ -0,0 +1,21 @@
+
+import kotlin.math.PI
+import kotlin.math.max
+import kotlin.math.min
+//import kotlin.math.*
+
+fun main() {
+    val isAdult = false
+    val isFemale = true
+    val isCompleted: Boolean? = null // this is a boolean value but it can be null
+//    possible value for Boolean is true false and null
+    println(isCompleted)
+
+//    logical operator
+    println(isAdult && isFemale)
+    println(isAdult || isFemale)
+    println(!isAdult)
+
+
+
+}
---
+++ src/PlusSign.kt
@@ -0,0 +1,20 @@
+
+import kotlin.math.PI
+import kotlin.math.max
+import kotlin.math.min
+//import kotlin.math.*
+
+fun main() {
+//    polymorphism for the +
+    val numSum = 10 + 10
+    println(numSum)
+    val stringSum = "10" + "10"
+    println(stringSum)
+
+    val number = "10"
+    println(10 + number.toInt())
+
+    val number2 = 10
+    println(10.toString() + "100") // press ctrl fn space will give the hint for the bare number
+
+}
