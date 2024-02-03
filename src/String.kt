0a0284029dfa8a28a9ff62ca73d7f417ee525008
Qian Tang <qian@itrazotracetech.com>
2024-02-03 21:23:21 +1100

    string multiple line

--- src/String.kt
+++ src/String.kt
@@ -3,7 +3,21 @@ fun main() {
     val age = 90
 
 //    if need any operation or call function, need to add {}
-    val msg = "Name $name, Age: ${age+1}"
+    val msg = "Name $name, Age: ${age+1}, %s"
     println(msg)
+
+//    multiple line string
+//    if will keep the indentation of the string
+//    %s is the replacement for argument of .format function
+//    format function can be used for a normal string too
+    val email = """
+        Hello %s
+        How
+          Are
+        You
+    """.trimIndent()
+
+    println(email.format("Amy"))
+    println(msg.format("OKK"))
 
 }
