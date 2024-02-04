7f63519c6ab6ddf5800c2caf12cd48f856d0070c
Qian Tang <qian@itrazotracetech.com>
2024-02-05 00:22:58 +1100

    when expre2

--- src/WhenExpression.kt
+++ src/WhenExpression.kt
@@ -17,7 +17,28 @@ fun main() {
             "Unknown"
         }
     }
-
     println(g)
+
+
+//    if statement can be changed to when
+    val n1 = 100
+    val n2 = 20
+    when {
+        (n1>n2) -> println(":)")
+        (n1==100) -> println("==100")
+        else -> {
+            println("Nothing")
+        }
+    }
+
+//    can use range in when
+    val age = 18
+    when(age) {
+//        just use .. to express range
+        in 13..19 -> println("teenager")
+        !in 0..12 -> println("kids")
+        else -> println("Adult")
+    }
+
 
 }
