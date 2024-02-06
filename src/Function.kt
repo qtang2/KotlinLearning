b8d8e1d4b1a4d2cb8ab27ac6fcf5b74dcc6731d1
Qian Tang <qian@itrazotracetech.com>
2024-02-06 21:22:46 +1100

    function as args

--- src/Function.kt
+++ src/Function.kt
@@ -3,6 +3,20 @@ fun main() {
 //    explicit specify the argument
     greet(age=10, name="AmySis")
     greet( name="Aya")
+
+    foo( bar = {
+        println("bar func 1")
+    })
+    foo( bar = {
+        println("bar func 2")
+    })
+
+//    can invoke the foo function without () as below
+//    but this will only work when the function argument is the last argument
+    foo {
+        println("bar function 3")
+    }
+    foo()
 }
 //Unit is equal to void in Java
 fun greet(name: String, age:Int = -1): Unit {
@@ -15,4 +29,10 @@ fun greet(name: String, age:Int = -1): Unit {
     }else {
         println("is not adult")
     }
+}
+
+//pass function as argument
+fun foo(bar: () -> Unit = {} ) {
+    println("bar function-------")
+    bar()
 }
