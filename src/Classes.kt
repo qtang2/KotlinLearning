f62ef76195bdc8af24f4cb8ee874be93b87ccda5
Qian Tang <qian@itrazotracetech.com>
2024-02-06 22:33:41 +1100

    properties and methods

--- src/Classes.kt
+++ src/Classes.kt
@@ -3,17 +3,47 @@ fun main() {
     val tv = SmartDevice()
     tv.price = 1000.0
     tv.brand = "TCL"
+    println("tv ${tv.brand}, ${tv.price}")
     val phone = SmartDevice()
-    tv.price = 100.0
-    tv.brand = "TCL"
+    phone.price = 100.0
+    phone.brand = "Apple"
 //    tv.price1 = 10.0 cannot do this as val is immutable
+    println("phone ${phone.brand}, ${phone.price}")
+    println("phone state ${phone.getDeviceState()}")
+    phone.turnOn()
+    println("phone state ${phone.getDeviceState()}")
+    
+
 }
 
 //blueprint
 class SmartDevice {
 //    properties - variables
     var brand: String = ""
+////    default getter: get() = field
+//        get() = field
+//        get() = "foo"
+////    default getter
+//        set(value) {
+//            field = value
+//        }
     var price: Double = 0.0
-    val price1: Double = 0.0
+//        get() = "foo"
+//    val price1: Double = 0.0
+
+    var isSwitchedOn: Boolean = false
+
+    fun getDeviceState() = isSwitchedOn
+
 //    behaviors - functions
+    fun turnOn() {
+        isSwitchedOn = true
+        println("$brand Turn on")
+    }
+    fun turnOff() {
+        isSwitchedOn = false
+        println("$brand Turn Off")
+    }
+
+
 }
